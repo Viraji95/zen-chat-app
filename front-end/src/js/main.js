aa70747e1cdb676970d01ed598eb18b5dcf031b5
Virajisesiri <viraji.sesiri@gmail.com>
2024-01-19 15:22:31 +0530

    Update the app

--- front-end/src/js/main.js
+++ front-end/src/js/main.js
@@ -2,3 +2,38 @@
 
 const txtMessageElm = document.querySelector("#txt-message");
 const btnSendElm = document.querySelector("#btn-send");
+const outputElm = document.querySelector("#output");
+const { API_BASE_URL } = process.env;
+
+btnSendElm.addEventListener('click', () =>{
+    alert('okay');
+    const message = txtMessageElm.value.trim();
+    if(!message) return;
+
+    fetch(`${API_BASE_URL}/messages`, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(message)
+    }).then(res => {
+        if(res.ok){
+            //addChatMessageRecord(msgObj);
+            outputElm.scrollTo(0, outputElm.scrollHeight);
+            txtMessageElm.value = '';
+            txtMessageElm.focus();
+            
+        }else {
+            alert("Failed to send the message, please try again");
+        }
+    }).catch(err => alert("Failed to connect with the server, please try again later"))
+});
+
+function addChatMessageRecord({message, email}) {
+    const messageElm = document.createElement('div');
+    messageElm.classList.add('message')
+
+    outputElm.append(messageElm);
+    messageElm.innerText = message;
+    
+}
