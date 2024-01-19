6191cc0f7325c001f7e53007efb5c1ef53533ec9
Virajisesiri <viraji.sesiri@gmail.com>
2024-01-19 13:34:46 +0530

    Update MessageTO

--- back-end/src/main/java/lk/ijse/dep11/app/to/MessageTO.java
+++ back-end/src/main/java/lk/ijse/dep11/app/to/MessageTO.java
@@ -1,4 +1,21 @@
 package lk.ijse.dep11.app.to;
 
-public class MessageTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageTO  implements Serializable {
+    @NotBlank(message = "Chat message can't be empty")
+    private String message;
+    @NotEmpty(message = "Email can't be empty")
+    @Email(message = "Invalid email")
+    private String email;
 }
