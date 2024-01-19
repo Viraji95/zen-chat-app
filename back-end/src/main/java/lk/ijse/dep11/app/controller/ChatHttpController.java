053a042a5b84fe2616f749a85978d89d97356a39
Virajisesiri <viraji.sesiri@gmail.com>
2024-01-19 13:26:53 +0530

    Update ChatHttpController

--- back-end/src/main/java/lk/ijse/dep11/app/controller/ChatHttpController.java
+++ back-end/src/main/java/lk/ijse/dep11/app/controller/ChatHttpController.java
@@ -3,7 +3,9 @@ package lk.ijse.dep11.app.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.ConstraintViolationException;
 import java.util.List;
 import java.util.Map;
 import java.util.Vector;
@@ -14,6 +16,14 @@ import java.util.Vector;
 public class ChatHttpController {
 
     private final List<String> chatMessages = new Vector<>();
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public void validationExceptionHandler(ConstraintViolationException exp){
+        ResponseStatusException resExp = new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                exp.getMessage());
+        exp.initCause(resExp);
+        throw resExp;
+    }
 
     @GetMapping(produces = "application/json")
     public List<String> retrieveMessages() {
