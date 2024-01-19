fbd522b5f3001f1886bc7f0900acb776c5d025ea
Virajisesiri <viraji.sesiri@gmail.com>
2024-01-19 13:08:30 +0530

    Update app

--- back-end/src/main/java/lk/ijse/dep11/app/WebAppConfig.java
+++ back-end/src/main/java/lk/ijse/dep11/app/WebAppConfig.java
@@ -3,10 +3,12 @@ package lk.ijse.dep11.app;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
 @ComponentScan
 @Configuration
+@PropertySource("classpath:/application.properties")
 public class WebAppConfig {
 }
