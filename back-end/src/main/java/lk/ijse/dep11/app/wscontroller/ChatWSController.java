002d7e529053b37d75047ebb65c137bde3c09807
Virajisesiri <viraji.sesiri@gmail.com>
2024-01-20 09:38:57 +0530

    Finalize the back-end

--- back-end/src/main/java/lk/ijse/dep11/app/wscontroller/ChatWSController.java
+++ back-end/src/main/java/lk/ijse/dep11/app/wscontroller/ChatWSController.java
@@ -4,13 +4,16 @@ import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lk.ijse.dep11.app.to.MessageTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import javax.validation.ConstraintViolation;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class ChatWSController extends TextWebSocketHandler {
 
@@ -18,6 +21,9 @@ public class ChatWSController extends TextWebSocketHandler {
 
     @Autowired
     private ObjectMapper mapper;
+
+    @Autowired
+    private LocalValidatorFactoryBean localValidatorFactoryBean;
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         webSocketSessionsList.add(session);
@@ -31,7 +37,19 @@ public class ChatWSController extends TextWebSocketHandler {
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         try {
-            mapper.readValue(message.getPayload(), MessageTO.class);
+            MessageTO messageObj = mapper.readValue(message.getPayload(), MessageTO.class);
+            Set<ConstraintViolation<MessageTO>> violations = localValidatorFactoryBean.getValidator().validate(messageObj);
+            if(violations.isEmpty()) {
+                for (WebSocketSession webSocketSession : webSocketSessionsList) {
+                    if(webSocketSession == session) continue;
+                    if(webSocketSession.isOpen()) {
+                        webSocketSession.sendMessage(new TextMessage(message.getPayload()));
+                    }
+
+                }
+            }else {
+                session.sendMessage(new TextMessage("Invalid Message schema"));
+            }
         }catch (JacksonException exp){
             session.sendMessage(new TextMessage("Invalid JSON"));
         }
